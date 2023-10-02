8e40f3a8e74f20905bd9296303417aa4c088ee7a
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2023-10-02 20:56:28 +0200

    Add function newDiv to create a n * n grid This js function can create a x number of square shaped divs which auto adjust both height and width to fit into the HTML div container.

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const container = document.querySelector("#container");
 
-numOfDivs = 16
 
+let num = 16;
+let sqNum = num * num;
+
+function newDiv(sqNum) {
+    for (i = 0; i < sqNum; i++) {
+        let content = document.createElement("div");
+        content.classList.add("square");
+        content.style.height = (720 / num) + "px";
+        content.style.width = (720 / num) + "px";
+        container.appendChild(content);
+    }
+};
+
+window.onload = newDiv(sqNum); 

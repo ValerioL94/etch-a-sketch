ffbf3ed9fb4cc0f4cc4580ae42d0f64465345ce8
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2023-10-03 14:14:12 +0200

    Add full functioning slider The slider will now display the number of squares for each side of the grid and modify the grid itself in real time.

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 const grid = document.querySelector("#grid");
-
 const slider = document.getElementById("myRange");
 const output = document.getElementById("selection");
-output.textContent = slider.value + "x" + slider.value;
-let sqNum = slider.value * slider.value;
-
-slider.addEventListener("input", function () {
-    output.textContent = this.value + "x" + this.value;
-});
-
-
+output.textContent = (slider.value + "x" + slider.value);
+let sqNum = (slider.value * slider.value);
 
 function newGrid(sqNum) {
     for (i = 0; i < sqNum; i++) {
         let content = document.createElement("div");
         content.classList.add("square");
-        content.style.height = (720 / slider.value) + "px";
-        content.style.width = (720 / slider.value) + "px";
+        content.style.height = ((720 / slider.value) + "px");
+        content.style.width = ((720 / slider.value) + "px");
         content.addEventListener("mouseover", function (e) {
             e.target.style.backgroundColor = "black"
         });
         grid.appendChild(content);
     }
 };
+
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+slider.addEventListener("input", function () {
+    output.textContent = (this.value + "x" + this.value);
+});
+slider.addEventListener("input", function () {
+    removeChildNodes(grid);
+    newGrid((this.value * this.value));
+})
+
 
 
 /*
@@ -32,5 +40,4 @@ squares.forEach((square) => {
         e.target.style.backgroundColor = "black";
     })
 }); */
-
-newGrid(sqNum); 
+newGrid(sqNum);

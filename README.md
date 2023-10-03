bd4499db87b5d0efd7271bb2125b04d031b1658b
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2023-10-03 13:24:34 +0200

    Add temporary grid slider and option buttons

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Etch-a-Sketch
+
+Building a browser version of etch a sketch using js DOM manipulation.
--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-const container = document.querySelector("#container");
+const grid = document.querySelector("#grid");
+
+const slider = document.getElementById("myRange");
+const output = document.getElementById("selection");
+output.textContent = slider.value + "x" + slider.value;
+let sqNum = slider.value * slider.value;
+
+slider.addEventListener("input", function () {
+    output.textContent = this.value + "x" + this.value;
+});
 
 
-let num = 16;
-let sqNum = num * num;
 
-function newDiv(sqNum) {
+function newGrid(sqNum) {
     for (i = 0; i < sqNum; i++) {
         let content = document.createElement("div");
         content.classList.add("square");
-        content.style.height = (720 / num) + "px";
-        content.style.width = (720 / num) + "px";
-        container.appendChild(content);
+        content.style.height = (720 / slider.value) + "px";
+        content.style.width = (720 / slider.value) + "px";
+        content.addEventListener("mouseover", function (e) {
+            e.target.style.backgroundColor = "black"
+        });
+        grid.appendChild(content);
     }
 };
 
-window.onload = newDiv(sqNum); 
+
+/*
+const squares = document.querySelectorAll("square");
+squares.forEach((square) => {
+    square.addEventListener("mouseover", function (e) {
+        e.target.style.backgroundColor = "black";
+    })
+}); */
+
+newGrid(sqNum); 
